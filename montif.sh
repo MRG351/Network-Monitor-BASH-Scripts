4c9c91c400d85b95ba05e4f79e81d342f268f8c8
MRG351 <matthewryangrasso@gmail.com>
2017-03-04 05:11:03 -0500

    Update montif.sh
    minor syntax updates (changed to resemble the montall.sh variable names) and added some comments.

--- montif.sh
+++ montif.sh
@@ -5,6 +5,7 @@
 IFS=( 'eth0_0' 'eth1_0' 'eth1_1' 'eth1_2' 'eth1_3') # maybe read these from file or args
 VALID_ARG=0
 
+# check that program argument matches one of the valid interface identifiers above
 for i in "${IFS[@]}"
 	do
 		if [[ $1 == $i ]]; then
@@ -12,30 +13,33 @@ for i in "${IFS[@]}"
 		fi
 	done
 
+# if argument doesn't match a valid interface identifier, exit with an error
 if [ $VALID_ARG == 0 ]; then
 	echo "error: interface identifier invalid"
 	exit 255
 fi
 
-UP=1
+UP=1 # assume interface is UP at program start
 while true
 	do
-		RESULT=$("ip" "link" "show" "dev" "$1")
+		OUTPUT=$("ip" "link" "show" "dev" "$1")
 		EXIT_STATUS=$?
 
+		# following is executed if interface goes DOWN from UP
 		if [ $UP == 1 ]; then
-			if [[ $RESULT == *"DOWN"* ]]; then
-				echo "$(date)"": ""$RESULT"
+			if [[ $OUTPUT == *"DOWN"* ]]; then
+				echo "$(date)"": ""$OUTPUT"
 				UP=0
 			fi
 		elif [ $UP == 0 ]; then
-			if [[ $RESULT == *"UP"*  ]]; then
-				echo "$(date)"": ""$RESULT"
+			# following is executed if interface goes UP from DOWN
+			if [[ $OUTPUT == *"UP"*  ]]; then
+				echo "$(date)"": ""$OUTPUT"
 				UP=1
 			fi
 		fi
 
-		sleep 5
+		sleep 5 # monitor interface every 5 seconds
 	done
 
 exit 0
